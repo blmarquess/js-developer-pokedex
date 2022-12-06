0c25a627748f43d7c728daf6f9511f4949a12998
Bruno Marques <blmarques.dev@gmail.com>
2022-12-05 22:16:27 -0300

    refactor(model): create constructor to eazy create new objects

---
+++ assets/js/pokemon-model.mjs
@@ -0,0 +1,9 @@
+export default class Pokemon {
+  constructor(number, name, type, types, photo) {
+    this.number = number;
+    this.name = name;
+    this.type = type;
+    this.types = types;
+    this.photo = photo;
+  }
+}
